72fce1da7e6942015d60a773113cd2e7c11cc06f
Enzo Teixeira <enzo_jesus@icloud.com>
2018-08-20 17:58:15 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+        console.log({firstCard, secondCard});
+
+        // do cards natch?
+        if (firstCard.dataset.framework ===
+            secondCard.dataset.framework) {
+
+        }
+    }
+
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
